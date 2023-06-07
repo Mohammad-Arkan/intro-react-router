4aec8de6a566f923525da09fddd558b0ca64a156
ARKAN <a.r.khan.coxyboys@gmail.com>
2023-06-07 09:23:04 +0600

    first commit

---
+++ src/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>Share your information</h2>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import Home from './components/Home/Home.jsx';
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Default page</div>,
+//   },
+//   {
+//     path:"/about",
+//     element:<About></About>
+//   },
+//   {
+//     path:"/contact",
+//     element:<Contact></Contact>
+//   },
+// ]);
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element:<Home></Home>,
+    children:[
+      {
+        path:"/about",
+        element:<About></About>
+      },
+      {
+        path:"/contact",
+        element:<Contact></Contact>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
